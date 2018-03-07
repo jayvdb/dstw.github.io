f09b450ed75cf5c010a7bbfaf69589be96c099d7
Didik Setiawan <ds@didiksetiawan.com>
2018-03-08 02:04:43 +0700

    Second initial commit for CI testing purpose

--- script/cibuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Courtesy: https://jekyllrb.com/docs/continuous-integration/travis-ci/
-
-# halt script on error
-set -e
-
-bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external
